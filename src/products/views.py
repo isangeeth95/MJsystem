0ca84c7d5147597ed2ef9f3e533ff0a12d8d06ef
isangeeth95 <36575796+isangeeth95@users.noreply.github.com>
2019-03-07 20:56:28 +0530

    Changed the home page and edit the product list view

--- src/products/views.py
+++ src/products/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from inventory.models import jewelry , Necklaces
-from delivery.models import DeliveryInfo
+from inventory.models import jewelry , Necklaces, Ring
 
 # Create your views here.
 
 def Product_List_View(request):
-    #querySet = DeliveryInfo.objects.all()
-    querySet = Necklaces.objects.all()
+    querySet = Ring.objects.all()
     context = {
         'qs': querySet
     }
