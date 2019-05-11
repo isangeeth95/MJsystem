6fd80229faf0f95b04e59f45eed7080f872c2e2a
Pavithra <pavithra.blog@gmail.com>
2019-05-11 12:06:07 +0530

    create customer details view using ajax  - click view buttons in /dashboard/customer/

--- src/customer/urls.py
+++ src/customer/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
         path('', views.customer_page, name ='customer-handling'),
+
+        url(r'^info/(?P<pk>\d+)/$',views.customer_info, name='customer-info'),
 
 ]
--- src/customer/views.py
+++ src/customer/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Customer
+from billing.models import BillingProfile
+from order.models import Order
+
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+
 # from accounts.models import online_customer
 
 # Create your views here.
@@ -9,5 +15,13 @@ def customer_page(request):
     context = {
         'cus': cus,
         'dashboard_dir': 'Customer',
+        'orders': Order.objects.all()
     }
     return render(request,'customer/customerInfo.html', context)
+
+
+def customer_info(request, pk):
+    cus = get_object_or_404(Customer,pk=pk)
+    data = dict()
+    data['details1'] = render_to_string('customer/customerDetails.html',{'customer': cus})
+    return JsonResponse(data)
