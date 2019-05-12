40f8e586e10b26825a58b03082237da8be527537
isangeeth95 <36575796+isangeeth95@users.noreply.github.com>
2019-05-12 12:46:17 +0530

    admin view order list

--- src/order/urls.py
+++ src/order/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('',views.order, name ='blog-order'),
     url(r'^order_list/$', order_list_view, name='order_list_view'),
-    #url(r'^display_order',)
+    url(r'^order_list/dashboard/$', admin_view_orders, name='admin_view_orders'),
+    #url(r'^display_order',)admin_view_orders
 ]
--- src/order/views.py
+++ src/order/views.py
@@ -21,3 +21,13 @@ def order_list_view(request):
         }
     return render(request, "order/admin-get-order-list.html", context)
 
+
+def admin_view_orders(request):
+    if request.user.is_admin or request.user.is_staff:
+        print(request.user)
+        order_list = Order.objects.all()
+        context = {
+            'order_list': order_list,
+        }
+        return render(request, 'order/admin-get-order-list-dashboard.html', context)
+
