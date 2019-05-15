b2661e1b53ac471a44181f1f8b9e6e5fff89cc7c
isangeeth95 <36575796+isangeeth95@users.noreply.github.com>
2019-05-15 17:41:42 +0530

    search now function a little

--- src/search/views.py
+++ src/search/views.py
@@ -7,28 +7,21 @@ from django.db.models import Q
 
 class SearchProductView(ListView):
     template_name = "search/view.html"
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(SearchProductView, self).get_context_data(*args, **kwargs)
-    #     query = self.request.GET.get('q')
-    #     context['query'] = query
-    #     return context
-    #
-    # def get_queryset(self, *args, **kwargs):
-    #     request = self.request
-    #     method_dict = request.GET
-    #     query = method_dict.get('q', None)
-    #     if query is not None:
-    #         lookups = Q(category__icontains=query) | Q(description__icontains=query)
-    #         return jewelry.objects.filter(lookups).distinct()
-    #     return None
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        return context
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
-        # method_dict = request.GET
-        # query = method_dict.get('q', None)
-        # if query is not None:
-        #     lookups = Q(category__icontains=query) | Q(description__icontains=query)
-        #     return jewelry.objects.filter(lookups).distinct()
-        return jewelry.objects.filter(category__icontains='RING')
+        method_dict = request.GET
+        query = method_dict.get('q', None)
+        if query is not None:
+            lookups = Q(description__icontains=query)
+            return jewelry.objects.filter(lookups).distinct()
+        return None
+
 
 
